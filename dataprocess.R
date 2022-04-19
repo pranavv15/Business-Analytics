a9a802436431799a000cfa1e2cc6d499226ed876
pranavv15 <vinodpranav15@gmail.com>
2022-04-19 05:03:26 -0400

    Submission 4 - Random Forest : 8.69

--- dataprocess.R
+++ dataprocess.R
@@ -10,8 +10,9 @@ library(caret)
 library(e1071)
 library(lime)
 library(corrplot)
+library(neuralnet)
 par(mfrow = c(1,1))
-# Reading the data
+# Reading the datax
 data <- read.csv(file.choose(), header = T)
 data <- data[,-c(1)]
 summary(data)
@@ -36,34 +37,44 @@ corrplot(cor_matrix, method = 'color')
 # Let me try droppping all but one of the strongle correlated variables
 # So i will keep V3 and V8 and drop the rest until V15
 
-new <- data[,-c(3,4,5,6,8,9,10,11,12)]
+new <- data
+#[,-c(3,4,5,6,7,8,9,10,11,12,13,14)]
 # New cor plot
 cor_mat2 <- cor(new)
 corrplot(cor_mat2,method = 'color')
 
-# Let me try to run linear regression on this data
-library(mlbench)
-library(caret)
-library(e1071)
-library(lime)
+par(mfrow = c(3,3))
+hist(data$V3)
+hist(data$V4)
+hist(data$V5)
+hist(data$V6)
+hist(data$V7)
+hist(data$V8)
+hist(data$V9)
+hist(data$V10)
+hist(data$V11)
 
+################################################################################################
+
+# Let me try to run linear regression on this data
 mydata <- as.data.frame((new))
 set.seed(1234) 
 ind <- sample(2, nrow(mydata), replace = T, prob = c(0.8, 0.2))
 train <- mydata[ind == 1,]
+mean(train$score)
 test <- (mydata[ind == 2,])
-
+mean(test$score)
 # Scale training data
 train2 <- as.data.frame(scale(train))
 
 # Simple Linear Regression
-
 # Training
 mod <- lm(score~., data = train2)
 summary(mod)
 
 # Testing
-test_reg <- predict(mod, test)*sd(train2$score) + mean(train2$score)
+test_reg <- predict(mod, test)
+# *sd(train$score) + mean(train$score)
 plot(test_reg ~ test$score, main = 'Predicted Vs Actual Score - Test data')
 sqrt(mean((test$score - test_reg)^2))
 cor(test$score, test_reg) ^2
@@ -72,12 +83,13 @@ cor(test$score, test_reg) ^2
 
 # Boosting
 set.seed(1234)
+train2 <- scale(train)
 cvcontrol <- trainControl(method="repeatedcv", 
                           number = 5,
                           repeats = 2,
                           allowParallel=TRUE)
 boo <- train(score ~ ., 
-             data=train,
+             data=train2,
              method="xgbTree", 
              trControl=cvcontrol,
              tuneGrid = expand.grid(nrounds = 5000,
@@ -90,7 +102,7 @@ boo <- train(score ~ .,
 plot(varImp(boo))
 
 # Plot, RMSE, R-square
-bo <-  predict(boo,  test)
+bo <-  predict(boo,  test)*sd(test$score) + mean((test$score))
 plot(bo ~ test$score, main = 'Predicted Vs Actual MEDV - Test data')
 sqrt(mean((test$score - bo)^2))
 cor(test$score, bo) ^2
@@ -145,15 +157,44 @@ explanation <- explain( x = test[1:3,],
                         n_features = 5)
 plot_features(explanation)
 
+###############################################################################################
+maxs <- apply(train, 2, max) 
+mins <- apply(train, 2, min)
+train <- as.data.frame(scale(train, center = mins, 
+                              scale = maxs - mins))
 
+maxs <- apply(test, 2, max) 
+mins <- apply(test, 2, min)
+test <- as.data.frame(scale(test, center = mins, 
+                              scale = maxs - mins))
+
+nn <- neuralnet(score~., data=train, hidden = c(2,1),
+                linear.output = TRUE,
+                lifesign = "minimal",
+                rep = 3)
+plot(nn)
+
+output <- compute(nn, test[,-1])
+
+# Compute mean squared error
+pr.nn_ <- output$net.result * (max(mydata$score) - min(mydata$score)) + min(mydata$score)
+test.r <- (test$score) * (max(mydata$score) - min(mydata$score)) + min(mydata$score)
+MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(test)
+
+plot(test$score, pr.nn_, col = "red", main = 'Real vs Predicted')
+abline(0, 1, lwd = 2)
+###############################################################################################
 # Test data from website
 test_final <- read.csv(file.choose(), header = T)
-test_final <- test_final[,-c(3,4,5,6,8,9,10,11,12)]
+corr <- cor(test_final)
+corrplot(corr, method = 'color')
+#test_final <- test_final[,-c(3,4,5,6,8,9,10,11,12)]
 # Predictions from boosted tree
 # Plot, RMSE, R-square
-bo <-  predict(boo,  test_final[,-c(1)])
-
-submission <- data.frame(test_final$Id, bo)
+# bo <-  predict(boo,  test_final[,-c(1)])
+rf.reg <- randomForest(score~., data=train, mtry=19, importance=TRUE, xtest=test_final[,-c(1)])
+rf.reg$predicted
+submission <- data.frame(test_final$Id, rf.reg[["test"]][["predicted"]])
 colnames(submission) <- c('Id', 'Expected')
 
 setwd("~/Desktop")
